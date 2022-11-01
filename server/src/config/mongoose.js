a6cf4c90c117f4fbcc83ffa48e6c2799a53a5de5
Diego Eiras <eiras.lucio@gmail.com>
2022-11-01 10:12:40 -0300

    ⚠️ FIX: Change MongoDB URI

--- server/src/config/mongoose.js
+++ server/src/config/mongoose.js
@@ -1,3 +1,10 @@
 import mongoose from 'mongoose'
 
-mongoose.connect('mongodb://localhost:27017/chat')
+const ENVIROMENT = process.env.NODE_ENV
+const MONGODB_URI = process.env.MONGODB_URI
+
+mongoose.connect(
+  ENVIROMENT === 'development'
+    ? 'mongodb://localhost:27017/chat'
+    : MONGODB_URI
+)
